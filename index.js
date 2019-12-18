ee6336356fcc5ee8a009764290d04e9934311f6e
seongwon <abc7988se@gmail.com>
2019-12-18 17:31:55 +0900

    main cli file

---
+++ index.js
@@ -0,0 +1,18 @@
+const inquirer = require("inquirer");
+
+function addContractTest() {
+
+}
+
+function addSnapshotTest() {
+
+}
+
+async function run() {
+// check argument
+// if Contract test,
+// if Snapshot test-Capture phase
+// if Snapshot test-Playback phase
+}
+
+run();
